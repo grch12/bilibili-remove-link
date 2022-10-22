b6b1aafe556b0b3f13dbc28498536919e17f3509
grch12 <pym1212@126.com>
2022-10-22 13:09:37 +0800

    bug fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // @name         bilibili remove links
 // @license      MIT
 // @namespace    http://tampermonkey.net/
-// @version      0.31
+// @version      0.32
 // @description  自动去除阿B视频评论区中AI生成的搜索链接。
 // @author       Bilibili @显卡厨师
 // @match        www.bilibili.com/video/*
